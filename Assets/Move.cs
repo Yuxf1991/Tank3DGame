fc759fe3e0bbd3256a7d459b8385828a6f61b932
Yu Tracy <18768116202@163.com>
2016-12-21 17:54:23 +0800

    Tank

---
+++ Assets/Move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	public float MoveSpeed = 1.0f;
+	public float AngleSpeed = 30.0f;
+	public float MaxSpeed = 10.0f;
+	public float FSpeed = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float temp1=MoveSpeed, temp2=MaxSpeed;
+		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+			temp1 *= FSpeed;
+			temp2 *= FSpeed;
+		} else {
+		}
+
+		Vector3 direction = Input.GetAxis("Vertical") * transform.forward;
+		transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * AngleSpeed);
+		if (Mathf.Sqrt(GetComponent<Rigidbody>().velocity.sqrMagnitude) < temp2)
+		{
+			Debug.Log("done");
+			GetComponent<Rigidbody>().AddForce(direction.normalized * temp1, ForceMode.Acceleration);
+		}
+
+		Debug.Log(GetComponent<Rigidbody>().velocity);
+
+	}
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+	float BulletRate = 0;
+	public GameObject BulletPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		BulletRate -= Time.deltaTime;
+		if ( BulletRate <= 0 )  
+		{  
+			BulletRate = 0.2f;  
+
+			if ( Input.GetKey( KeyCode.Space ) || Input.GetMouseButton(0) )  
+			{  
+				GameObject go= (GameObject)Instantiate(BulletPrefab);
+				go.transform.position = transform.position;
+				go.transform.rotation = transform.rotation;
+				go.transform.Rotate(0, 90, 0);
+			}  
+		}
+		
+	}
+}
